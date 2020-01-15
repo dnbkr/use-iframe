23afc79419ef5036235222376e5a561967d18ee5
Daniel Baker <danbaker@me.com>
2020-01-15 15:29:22 +0000

    support for multiple iframes. better message routing.

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import {
   useEffect,
   useState,
   Dispatch,
-  SetStateAction
+  SetStateAction,
+  useRef,
+  useMemo
 } from "react";
 
 interface Options {
   ref?: RefObject<HTMLIFrameElement>;
-  skip?: boolean;
 }
 
 type MessageDispatcher<Message> = (message: Message) => void;
@@ -21,18 +22,30 @@ export type MessageHandler<Message> = (
   dispatch: MessageDispatcher<Message>
 ) => void;
 
-const packetId = "__fromUseIframeHook";
-
-function encodeMessage<Message>(message: Message) {
-  return JSON.stringify({ payload: JSON.stringify(message), [packetId]: true });
+interface PrivateMessage {
+  __private: true;
+  type: "mounted";
+  id: string;
 }
 
-function decodeMessage<Message>(raw: any): Message | undefined {
+const packetId = "__fromUseIframeHook";
+
+function encodeMessage<Message>(message: Message, fromId?: string) {
+  return JSON.stringify({
+    fromId,
+    payload: JSON.stringify(message),
+    [packetId]: true
+  });
+}
+
+function decodeMessage<Message>(
+  raw: any
+): [Message, string | undefined] | undefined {
   try {
     const decoded = JSON.parse(raw);
     if (packetId in decoded && decoded[packetId] === true) {
       const message = JSON.parse(decoded.payload) as Message;
-      return message;
+      return [message, decoded.fromId];
     }
   } catch (e) {
     return undefined;
@@ -40,45 +53,117 @@ function decodeMessage<Message>(raw: any): Message | undefined {
   return undefined;
 }
 
+function isPrivateMessage(message: any): message is PrivateMessage {
+  return (
+    message.hasOwnProperty("__private") &&
+    "__private" in message &&
+    message.__private === true
+  );
+}
+
 export function useIframe<Message = any>(
   handleMessage: MessageHandler<Message>,
   options?: Options
 ): UseIframeResult<Message> {
-  const skip = options?.skip === true
+  const ref = options?.ref;
 
-  const isParent = options?.ref !== undefined;
+  const isParent = useMemo(() => ref !== undefined, [ref]);
 
-  const dispatch = useCallback(
-    (message: Message) => {
-      if (isParent && options?.ref?.current) {
-        options.ref.current.contentWindow?.postMessage(
-          encodeMessage(message),
-          "*"
-        );
-      } else if (isParent === false) {
-        window.parent.postMessage(encodeMessage(message), "*");
+  /** Scope: Parent */
+  const iframeId = ref?.current?.id;
+
+  const [queue, setQueue] = useState<(Message | PrivateMessage)[]>([]);
+
+  const [childId, setChildId] = useState<string>();
+
+  /** Scope Parent */
+  const isChildMounted = childId !== undefined;
+
+  const dispatch = useCallback((message: Message) => {
+    setQueue(q => [...q, message]);
+  }, []);
+
+  const dispatchPrivate = useCallback((message: PrivateMessage) => {
+    setQueue(q => [...q, message]);
+  }, []);
+
+  const handlePrivateMessage = useCallback(
+    (message: PrivateMessage) => {
+      switch (message.type) {
+        case "mounted":
+          if (message.id === iframeId) {
+            return setChildId(message.id);
+          }
       }
     },
-    [isParent, options]
+    [iframeId]
   );
 
-  const eventListener = useCallback(
-    (event: MessageEvent) => {
-      const message = decodeMessage<Message>(event.data);
-      if (message) {
-        handleMessage(message, dispatch);
+  const postMessage = useCallback(
+    (message: Message | PrivateMessage) => {
+      if (isParent && ref?.current) {
+        ref.current.contentWindow?.postMessage(encodeMessage(message), "*");
+      } else if (isParent === false) {
+        window.parent.postMessage(encodeMessage(message, childId), "*");
       }
     },
-    [handleMessage, dispatch]
+    [isParent, childId, ref]
   );
 
   useEffect(() => {
-    if (skip) {
-      return
+    const message = queue[0];
+    if (isParent) {
+      if (message && isChildMounted) {
+        setQueue(q => q.slice(1));
+        postMessage(message);
+      }
+    } else {
+      if (message) {
+        setQueue(q => q.slice(1));
+        postMessage(message);
+      }
     }
-    window.addEventListener("message", eventListener);
-    return () => window.removeEventListener("message", eventListener);
-  }, [skip, eventListener]);
+  }, [isParent, isChildMounted, postMessage, queue]);
+
+  const eventListener = useCallback(
+    (event: MessageEvent) => {
+      const [message, fromId] =
+        decodeMessage<Message | PrivateMessage>(event.data) || [];
+      if (message) {
+        if (isPrivateMessage(message)) {
+          handlePrivateMessage(message);
+        } else {
+          if (isParent) {
+            if (iframeId === fromId) {
+              handleMessage(message, dispatch);
+            }
+          } else {
+            handleMessage(message, dispatch);
+          }
+        }
+      }
+    },
+    [handlePrivateMessage, isParent, iframeId, handleMessage, dispatch]
+  );
+
+  useEventListener(eventListener);
+
+  useEffect(() => {
+    if (!isParent) {
+      const iframes = Array.from(
+        window.parent.document.getElementsByTagName("iframe")
+      );
+      const iframe = iframes.find(iframe => iframe.contentWindow === window);
+      if (iframe) {
+        dispatchPrivate({ type: "mounted", id: iframe.id, __private: true });
+        setChildId(iframe.id);
+      }
+    }
+  }, [dispatchPrivate, isParent]);
+
+  if (isParent && ref?.current && !iframeId) {
+    throw new Error("iframes must have an ID set");
+  }
 
   return [dispatch];
 }
@@ -106,32 +191,22 @@ export function useIframeEvent<EventName = string>(
   return [onEventHandler];
 }
 
-type SharedStateMessageType<S> =
-  | { type: "__private_mounted" }
-  | { type: "__private_unmounted" }
-  | { type: "__private_set-state"; state: S };
+type SharedStateMessageType<S> = { type: "__private_set-state"; state: S };
 
 export function useIframeSharedState<S>(
   initialState: S | (() => S),
   options?: Options
 ): [S, Dispatch<SetStateAction<S>>] {
-  const skip = options?.skip === true
-
   const [localState, setLocalState] = useState<S>(initialState);
-  const [mounted, setMounted] = useState(false);
 
   const handler: MessageHandler<SharedStateMessageType<S>> = useCallback(
     message => {
       switch (message.type) {
-        case "__private_mounted":
-          return setMounted(true);
-        case "__private_unmounted":
-          return setMounted(false);
         case "__private_set-state":
           return setLocalState(message.state);
       }
     },
-    [setMounted]
+    []
   );
 
   const isParent = options?.ref !== undefined;
@@ -139,24 +214,34 @@ export function useIframeSharedState<S>(
   const [dispatch] = useIframe(handler, options);
 
   useEffect(() => {
-    if (skip) {
-      return
-    }
-    if (!isParent) {
-      dispatch({ type: `__private_mounted` });
-      return () => dispatch({ type: "__private_unmounted" });
-    }
-    return;
-  }, [skip, isParent, dispatch]);
-
-  useEffect(() => {
-    if (skip) {
-      return
-    }
-    if (isParent && mounted) {
+    if (isParent) {
       dispatch({ type: "__private_set-state", state: localState });
     }
-  }, [skip, isParent, mounted, localState, dispatch]);
+  }, [isParent, localState, dispatch]);
 
   return [localState, setLocalState];
+}
+
+type MessageEventListener = (event: MessageEvent) => void;
+
+function useEventListener(
+  handler: MessageEventListener,
+  element: HTMLElement | Window = window
+) {
+  const savedHandler = useRef<MessageEventListener>();
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const isSupported = element && element.addEventListener;
+    if (!isSupported) return;
+    const eventListener = (event: Event) =>
+      savedHandler.current?.(event as MessageEvent);
+    element.addEventListener("message", eventListener);
+    return () => {
+      element.removeEventListener("message", eventListener);
+    };
+  }, [element]);
 }
